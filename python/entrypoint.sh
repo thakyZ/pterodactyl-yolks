fd1980a9abb1adc315f5285934ef59136179b7fb
Neko Boi Nick <thakyz@outlook.com>
2021-10-17 16:04:49 -0700

    Update entrypoint.sh

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-cd /home/container
+cd /home/container || exit 1
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+export INTERNAL_IP
 
 # Print current Python version
 python --version
 
+# Replace variables in the startup command
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo "${STARTUP@Q}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the startup command
+# Run the Server
 # shellcheck disable=SC2086
 exec env ${MODIFIED_STARTUP}
