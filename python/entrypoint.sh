f163aabb0cea28b8f136fa32e7e33f26622accde
Neko Boi Nick <thakyz@outlook.com>
2021-10-17 15:32:12 -0700

    Update entrypoint.sh

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -8,8 +8,9 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 python --version
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
-# Run the Server
-eval ${MODIFIED_STARTUP}
+# Run the startup command
+# shellcheck disable=SC2086
+exec env ${PARSED}
