7befafb6ebf804fb3a187217bb6ed4f5dd8a110c
Neko Boi Nick <thakyz@outlook.com>
2021-10-17 20:44:39 -0700

    Update entrypoint.sh

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 # Borrowed partially from:
-# https://github.com/matthewpi/images/blob/master/python/entrypoint.sh
+# https://github.com/parkervcp/yolks/blob/master/python/entrypoint.sh
 
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
-
-# Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
-export INTERNAL_IP
-
-# Switch to the container's working directory
 cd /home/container || exit 1
 
-# Print Python version
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+
+# Print current Python version
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpython --version\n"
 python --version
 
-# Replace variables in the startup command
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$MODIFIED_STARTUP"
 
-# Run the startup command
-# shellcheck disable=SC2086
-eval ${PARSED}
+# Run the Server
+eval ${MODIFIED_STARTUP}
