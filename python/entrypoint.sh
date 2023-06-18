ee1c5e12a2eedb0135423de2e692670b35e949ce
Neko Boi Nick <950594+thakyZ@users.noreply.github.com>
2023-06-18 07:29:48 -0700

    Update entrypoint.sh

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 # Borrowed partially from:
 # https://github.com/parkervcp/yolks/blob/master/python/entrypoint.sh
-
-cd /home/container || exit 1
+cd /home/container
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=$(ip route get 1 | awk '{print $NF;exit}')
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
 
 # Print current Python version
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpython --version\n"
 python --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$MODIFIED_STARTUP"
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
 eval ${MODIFIED_STARTUP}
