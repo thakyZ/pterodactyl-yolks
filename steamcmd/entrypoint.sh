aa4933610dfa69aef58ea8246c6c0cf295ae7f1e
Neko Boi Nick <950594+thakyZ@users.noreply.github.com>
2024-02-10 11:40:33 -0800

    Update entrypoint.sh

--- steamcmd/entrypoint.sh
+++ steamcmd/entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-# Wait for the container to fully initialize
-sleep 1
 # Switch to the container's working directory
 cd /home/container || exit 1
-
-# Default the TZ environment variable to UTC.
-TZ=${TZ:-UTC}
-export TZ
 
 # Information output
 echo "Running on Debian $(cat /etc/debian_version)"
@@ -54,7 +48,34 @@ echo "First launch will throw some errors. Ignore them"
 
 mkdir -p $WINEPREFIX
 
-WINETRICKS_RUN=("atmlib" "corefonts" "gdiplus" "msxml3" "msxml6" "vcrun2008" "vcrun2010" "vcrun2012" "fontsmooth-rgb" "gecko" "vcrun2022")
+# Check if wine-gecko required and install it if so
+if [[ $WINETRICKS_RUN =~ gecko ]]; then
+        echo "Installing Gecko"
+        WINETRICKS_RUN=${WINETRICKS_RUN/gecko}
+
+        if [ ! -f "$WINEPREFIX/gecko_x86.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86.msi
+        fi
+
+        if [ ! -f "$WINEPREFIX/gecko_x86_64.msi" ]; then
+                wget -q -O $WINEPREFIX/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47.4/wine_gecko-2.47.4-x86_64.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/gecko_x86.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_install.log
+        wine msiexec /i $WINEPREFIX/gecko_x86_64.msi /qn /quiet /norestart /log $WINEPREFIX/gecko_x86_64_install.log
+fi
+
+# Check if wine-mono required and install it if so
+if [[ $WINETRICKS_RUN =~ mono ]]; then
+        echo "Installing mono"
+        WINETRICKS_RUN=${WINETRICKS_RUN/mono}
+
+        if [ ! -f "$WINEPREFIX/mono.msi" ]; then
+                wget -q -O $WINEPREFIX/mono.msi https://dl.winehq.org/wine/wine-mono/8.0.0/wine-mono-8.0.0-x86.msi
+        fi
+
+        wine msiexec /i $WINEPREFIX/mono.msi /qn /quiet /norestart /log $WINEPREFIX/mono_install.log
+fi
 
 # List and install other packages
 for trick in "${WINETRICKS_RUN[@]}"; do
@@ -62,9 +83,15 @@ for trick in "${WINETRICKS_RUN[@]}"; do
         winetricks -q "${trick}"
 done
 
+# List and install other packages
+for trick in $WINETRICKS_RUN; do
+        echo "Installing $trick"
+        winetricks -q $trick
+done
+
 # Replace Startup Variables
-MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval "${MODIFIED_STARTUP}"
+eval ${MODIFIED_STARTUP}
